4cbd4260f0a13e3e92c59d1c4c3713a050a414e7
Aareon Sullivan <askully13@gmail.com>
2023-12-03 09:24:30 -0600

    Format with black

--- tkinter_nav/errors.py
+++ tkinter_nav/errors.py
@@ -2,7 +2,7 @@ class EmptyPageListError(Exception):
     """Raised when the page list is empty"""
 
     def __init__(self):
-        self.msg = 'Page list must not be empty'
+        self.msg = "Page list must not be empty"
 
     def __str__(self):
         return repr(self.msg)
@@ -12,7 +12,7 @@ class PageNotFoundError(Exception):
     """Raised when a page is not found"""
 
     def __init__(self):
-        self.msg = 'No page with that name was found'
+        self.msg = "No page with that name was found"
 
     def __str__(self):
         return repr(self.msg)
--- tkinter_nav/page_container.py
+++ tkinter_nav/page_container.py
@@ -7,7 +7,7 @@ class Container(Frame):
     def __init__(self, root):
         Frame.__init__(self)
 
-        self.pack(side='top', fill='both', expand=True)
+        self.pack(side="top", fill="both", expand=True)
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
