262c206790641740c667d2ce4288ef6dc4ee767b
Aareon Sullivan <askully13@gmail.com>
2023-12-03 09:22:52 -0600

    Format with black

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 import setuptools
 
-with open('README.md', 'r') as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='tkinter-nav',
-    version='0.0.5',
-    author='Maximilien Zaleski',
-    author_email='maximilienzaleski@yahoo.com',
-    description='Lightweight navigation wrapper for Tkinter',
+    name="tkinter-nav",
+    version="0.0.5",
+    author=["Maximilien Zaleski", "Aareon Sullivan"],
+    author_email=["maximilienzaleski@yahoo.com", "askully13@gmail.com"],
+    description="Lightweight navigation wrapper for Tkinter",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/maxzaleski/tkinter-nav',
-    packages=setuptools.find_packages(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Aareon/tkinter-nav",
+    packages=setuptools.find_packages(),``
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: Apache Software License'
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT Software License",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
